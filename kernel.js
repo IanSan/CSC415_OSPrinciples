163473425cac20a3c1f1a87a189251e306026c5c
Kevin Valenzuela <kev.jval@gmail.com>
2016-03-03 12:52:10 -0800

    Resturcture kernel
    Program starts from html and no longer within kernel.js
    Add IO request management.
    Remove Queue definition (now in list.js).
    Remove PCB definition (now in process.js).
    
    All the commands for exec() need to be defined.

--- kernel.js
+++ kernel.js
@@ -1,157 +1,111 @@
-// generic singly linked list container
-function qCon(item, next) {
-    this.item = item;
-    this.next = next;
-};
+//process queue
+var pq = new Queue();
 
+//io request queue
+var fq = new Queue();
 
-// process queue
-var pq = {
-    head: new qCon(),  // ref to head qCon
-    tail: new qCon(),  // ref to tail qCon
-    length: 0,   // length
-    pop_front: function() {
-        var oldHead = this.head;
-        this.head = this.head.next;
-        this.head.prev = 0;
-        this.length--;
-        if(this.length === 0) {
-            this.tail = 0;
-        }
-        return oldHead.item;
-    },
-    push_back: function(pcb) {
-        if(this.length === 0) {
-            this.tail = new qCon(pcb, 0);
-            this.head = this.tail;
-        } else {
-            var oldTail = this.tail;
-            this.tail = new qCon(pcb, 0);
-            oldTail.next = this.tail;
-        }
-        this.length++;
-    }
-};
-
-
-// io request queue
-var fq = {
-    head: new qCon(),  // ref to head qCon
-    tail: new qCon(),  // ref to tail qCon
-    length: 0,   // length
-    pop_front: function() {
-        var oldHead = this.head;
-        this.head = this.head.next;
-        this.head.prev = 0;
-        this.length--;
-        if(this.length === 0) {
-            this.tail = 0;
-        }
-        return oldHead.item;
-    },
-    push_back: function(pcb) {
-        if(this.length === 0) {
-            this.tail = new qCon(pcb, 0);
-            this.head = this.tail;
-        } else {
-            var oldTail = this.tail;
-            this.tail = new qCon(pcb, 0);
-            oldTail.next = this.tail;
-        }
-        this.length++;
-    }
-};
-
-
-function variable(identifier, data) {
-    this.identifier = identifier;
-    this.data = data;
-}
-
-
-function fileStruct(fp, flags) {
-    this.fp = fp;
-    this.flags = flags;
-}
-
-
-// creates process control block object
-function pcb(program, state, pc, argc, argv) {
-    this.program = program;
-    this.state = state;
-    this.pc = pc;
-    this.pid = 0;   // unused positive integer
-    this.mode = 0;  // user/kernel mode
-    this.parent = 0;   //parent process
-    this.children = 0; //children
-    this.vars = 0; // array of variables
-    this.argc = argc;
-    this.argv = argv;
-    this.files = 0;
-};
-
-
-// creates I/O request 
-function ioreq(request, fp, filename) {
-    this.request = request;
-    this.data = 0;
-    this.fp = fp;
-    this.filename = filename;
-}
-
-// creates process
+//create new process
 function load(program) {
-    // create new pcb
-    log("load process");
-    pq.push_back(new pcb(program, "ready", 0, 0, 0));
-    log(pq.tail.item.program);
+    pq.push_back(new PCB(program, "ready", 0, undefined));
+    console.log("loaded process");
+    console.log(pq.tail.object.program);
 };
 
+//I/O request object
+function IORequest(task, pcb, fp, data) {
+    this.task = task;
+    this.pcb = pcb;     //ref of requesting process
+    this.fp = fp;       //file pointer
+    this.data = data;
+    this.done = false;  //boolean has this request been responded to?
+};
 
+//return data to I/O requester
+function ioreturn(ioreq) {
+    var argv = ioreq.pcb.program[ioreq.pcb.pc];
+    //update variable assignment
+    switch(argv[0]) {
+        case "open":
+            ioreq.pcb.currVarlist.setVar(argv[3], ioreq.fp);
+            break;
+        case "read":
+            ioreq.pcb.currVarlist.setVar(argv[3], ioreq.data);
+            break;
+        case "write":
+            break;
+        case "close":
+            break;
+        default:
+            console.log("IO Return Error");
+    }
+    //increment pc and set ready
+    ioreq.pcb.pc = ioreq.pcb.pc + 1;
+    ioreq.pcb.state = "ready";
+}
+
+
+//execute process instruction
 function exec(pcb) {
-    var code = pcb.program[pcb.pc];
-    var cmd = code[0];
+    var argv = pcb.program[pcb.pc];
+    var cmd = argv[0];
     switch(cmd) {
         case "open":
-            var filename = code[1];
-            var flags = code[2];
-            var fp = code[3];
-            if(pcb.state) {
-                
-                //incr pc
-            } else {
-                // give file to this process
-                pcb.files.push(new fileStruct(0, flags));
-                pcb.vars.push(new variable(fp, 0));
-                // enqueue file req
-                fq.push_back(new ioreq("open", fp, filename));
-                // save PCB
-                // changing to waiting
-            }
+            fq.push_back(new IORequest(cmd, pcb, undefined, argv[1]));
+            pcb.state = "waiting";
+            break;
+        case "read":
+            fq.push_back(new IORequest(cmd, pcb, argv[1]));
+            pcb.state = "waiting";
+            break;
+        case "write":
+            break;
+        case "close":
+            break;
+        case "set":
+            pcb.currVarlist.setVar(argv[1], argv[2]);
+            break;
+        case "add":
+            pcb.currVarlist.setVar(argv[1], pcb.currVarlist.getVar[argv[2]] + pcb.currVarlist.getVar[argv[3]]);
+            break;
+        default:
+            //ERROR
             break;
     }
 };
 
 
 function kernel() {
-    // load everything
+    console.log("kernel started");
+    //load everything
     load(p1);
     
-    // execute instructions of ready processes
+    //execute instructions of ready processes
     while(true) {
-        // run next ready proc in queue
-        while(pq.head.item.state === "ready") {
-            pq.head.item.state = "running";
-            exec(pq.head.item);
+        //run next ready proc in queue
+        while(!pq.isEmpty() && pq.front().state === "ready") {
+            exec(pq.front());
         }
-        // move non-ready process to end of queue
-        while(pq.head.state !== "ready") {
+        //move process to end of queue
+        if(!pq.isEmpty() && pq.front().state === "terminated") {
+            pq.pop_front();
+        } else {
             pq.push_back(pq.pop_front());
         }
-        // unload finished processes
+        
+        //unload finished processes
+        while(!pq.isEmpty() && pq.front().state === "terminated") {
+            pq.pop_front();
+        }
+        
+        //run io driver
+        while(io.ready && !fq.isEmpty() && !fq.front().done) {
+            iodriver(fq.front());
+        }
+        //return finished io requests
+        while(!fq.isEmpty() && fq.front().done) {
+            //return data to requesting process
+            //remove io request
+            ioreturn(fq.pop_front());
+        }
     }
 };
-
-
-log("start kernel");
-kernel();
