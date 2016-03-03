8ceba2b0ec6893f7fb54e38bf083f9a5873d6977
Kevin Valenzuela <kev.jval@gmail.com>
2016-03-03 12:43:39 -0800

    Add queue data structure

---
+++ list.js
@@ -0,0 +1,45 @@
+//generic doubly linked list container
+function Node(object, next, prev) {
+    this.object = object;
+    this.next = next;
+    this.prev = prev;
+}
+
+
+//FIFO queue
+function Queue() {
+    this.head;       //ref to head qCon
+    this.tail;       //ref to tail qCon
+    this.length = 0; //length
+}
+Queue.prototype.isEmpty = function() {
+    if(this.length > 0)
+        return false;
+    return true;
+};
+Queue.prototype.front = function() {
+    if(this.head === undefined)
+        return undefined;
+    return this.head.object;
+};
+Queue.prototype.pop_front = function() {
+    var oldHeadObject = this.head.object;
+    this.head = this.head.next;
+    this.length--;
+    //alternatively if length === 0
+    if(this.head === undefined) {
+        this.tail = undefined;
+    }
+    return oldHeadObject;
+};
+
+Queue.prototype.push_back = function(object) {
+    if(this.length === 0) {
+        this.tail = new Node(object);
+        this.head = this.tail;
+    } else {
+        this.tail.next = new Node(object);
+        this.tail = this.tail.next;
+    }
+    this.length++;
+};
