34d0c7d7ff6c50971774c6e934863cbad9c6a9c1
Kevin Valenzuela <kev.jval@gmail.com>
2016-03-18 10:38:12 -0700

    Change keyboard driver bahavior
    Replace previous logic where a new keyboard driver
    process would be created when a 'Enter' was
    pressed. Now, a single long-lived process controls
    manages input. This process sleeps and waits for
    'Enter' to be pressed.

--- KeyboardDriver.js
+++ KeyboardDriver.js
@@ -1,19 +1,32 @@
-//waits for a control character, then sends data
-fs.data["dev/input"] = "";
-
+//waits for a control character, then writes data into dev/input
 var keyboard = [
     [set, ["filename", "dev/input"]],
     [open, ["filename", "w", "fp"]],   //create device dev/input
+    //create textarea thats signal this process
     [function(pcb, argv) {
-            var doc = document.getElementById('keyboard');
+            var textarea = document.createElement("textarea");
+            textarea.id = "keyboard";
+            textarea.onkeypress =
+                    function(event) {
+                        if (event.keyCode === 13) { //wait for 'Enter' keypress
+                            pcb.state = "ready";    //wake up this process
+                        }
+                    };
+            var div = document.createElement("div");
+            div.appendChild(textarea);
+            document.body.appendChild(div);
+            pcb.state = "waiting";  //wait for keypress event
+    }, []],
+//instr #3
+    [function(pcb, argv) {
+            var doc = document.getElementById("keyboard");
             var str = doc.value;
-            doc.value = '';    //clear textarea
+            doc.value = "";    //clear textarea
             pcb.set("buffer", str);
     }, []],
     [write, ["fp", "buffer", 100]],
-    // need a way to signal this process to wake upon keypresses
-    /*[function(pcb, argv) {
-            pcb.pc = 0; //next instr 1 function
+    [function(pcb, argv) {
+            pcb.pc = 2; //next instr 3
             pcb.state = "waiting";
-    }, []]*/
+    }, []]
 ];
