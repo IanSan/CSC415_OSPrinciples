e84743bd9a205e4d2a6fc74d5717393e56941810
Kevin Valenzuela <kev.jval@gmail.com>
2016-03-18 21:57:12 -0700

    Add echo.js
    Prints arguments to stdout.

---
+++ echo.js
@@ -0,0 +1,15 @@
+fs.data["echo"] = [
+    [open, ["stdout", "w", "fp"]],
+    [set, ["buffer", ""]],
+    [function(pcb, argv) {
+            var args = pcb.get("argv");
+            args.shift();   //shifts out "echo"
+            args = args.join(" ");
+            args = args + "\n";
+            if (args.length > 0) {
+                pcb.set("buffer", args);
+            }
+    }, []],
+    [write, ["fp", "buffer"]],
+    [close, ["fp"]]
+];
