b616b4e32cc2716f66baa6a397d0d9755d305200
Kevin Valenzuela <kev.jval@gmail.com>
2016-03-18 18:38:54 -0700

    Fix process inheritance
    Fix PCB constructor to correctly initialize empty
    properties and inherit certain variables from the
    parent process. Pass argument vector to this new
    process.
    
    Add method createChild for creating children
    processes.

--- process.js
+++ process.js
@@ -30,22 +30,37 @@ varlist.prototype.getValue = function(identifier, scope) {
 
 
 //process control block object
-function PCB(program, state, pid, parent) {
+function PCB(program, state, pid, parent, argv) {
     this.program = program; //program code
     this.state = state;    //string process state
     this.pc = 0;            //integer program counter
     this.pid = pid;         //integer process id
     this.mode = 0;          //user/kernel mode
     this.parent = parent;   //parent process
-    this.children;          //children process
-    this.varlist = new varlist(
-            this.parent === undefined ? undefined : this.parent.currVarlist);
+    this.children = new Array();        //children process
+    this.varlist = new varlist();       //empty varlist
     this.currVarlist = this.varlist;    //ref to current scope
-    this.fileList = new Array();
+    this.initializeVarlist(argv);
+    this.fileList = 
+            this.parent === undefined ? new Array() : this.parent.fileList;
+    if (this.parent !== undefined) {
+        this.parent.children.push(this);
+    }
 }
 PCB.prototype.set = function(identifier, value) {
     return this.currVarlist.setValue(identifier, value);
 };
 PCB.prototype.get = function(identifier, value) {
     return this.currVarlist.getValue(identifier);
+};
+PCB.prototype.initializeVarlist = function(argv) {
+    this.set("argv", argv);
+    if (this.parent !== undefined) {
+        this.set("stdin", this.parent.get("stdin"));
+        this.set("stdout", this.parent.get("stdout"));
+        this.set("stderr", this.parent.get("stderr"));
+    }
+};
+PCB.prototype.createChild = function(program, state, pid, argv) {
+    return new PCB(program, state, pid, this, argv);
 };
