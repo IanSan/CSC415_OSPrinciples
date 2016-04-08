d032ece3bad6cdc24130fea16afb9ac4682b2912
Kevin Valenzuela <kev.jval@gmail.com>
2016-04-07 21:12:37 -0700

    Add put function
    put will be used for filling up the initial
    filesystem with files.

--- fs.js
+++ fs.js
@@ -8,6 +8,34 @@ var fs = {
         "dev": new FileObject("drw-rw-rw-", {}),
     }),
     
+    //puts data into contents of a new file at specificed path (absolute)
+    put: function(path, data) {
+        //resolve/create path
+        if (path[0] !== "/") {
+            //only absolute paths
+            return;
+        }
+        path = path.split("/");
+        var currDir = this.root;
+        var i = 1;
+        while (i < path.length - 1) {
+            //if name exists
+            if (path[i] in currDir.data) {
+                //check it is a directory
+                if (currDir.data[path[i]].meta[0] !== "d") {
+                    //path resolution failed
+                    return;
+                }
+            } else {
+                //create new directory
+                currDir.data[path[i]] = new FileObject("drw-rw-rw-", {});
+            }
+            currDir = currDir.data[path[i]];
+            i++;
+        }
+        currDir.data[path[i]] = new FileObject("-rw-rw-rw-", data);
+    },
+    
     /**
     *input: name of file 
     *outputs:filePointer, which is defined in file.js
