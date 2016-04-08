84d333bf5fb32375b820e391b8801bba3770b832
Kevin Valenzuela <kev.jval@gmail.com>
2016-04-07 22:19:38 -0700

    Fix fs methods to use new FilePointer

--- fs.js
+++ fs.js
@@ -31,13 +31,40 @@ var fs = {
         currDir.data[path[i]] = new FileObject("-rw-rw-rw-", data);
     },
     
+    //returns FileObject at specified path (absolute)
+    getFile: function(path) {
+        //resolve
+        if (path[0] !== "/") {
+            //only absolute paths
+            return undefined;
+        }
+        path = path.split("/");
+        var currDir = this.root;
+        var i = 1;
+        while (i < path.length - 1) {
+            //if directory exists
+            if (!(path[i] in currDir.data) ||
+                    (currDir.data[path[i]].meta[0] !== "d")) {
+                return undefined;
+            }
+            currDir = currDir.data[path[i]];
+            i++;
+        }
+        if (path[i] in currDir.data) {
+            return currDir.data[path[i]];
+        } else {
+            return undefined;
+        }
+    },
+    
     /**
     *input: name of file 
     *outputs:filePointer, which is defined in file.js
     */
     getFilePointer: function(filename) {
-        if(filename in fs.data) {
-            return new FilePointer(filename);
+        var file = this.getFile(filename);
+        if (file !== undefined) {
+            return new FilePointer(file);
         } else {
             //ERROR file not found
             return undefined;
@@ -49,23 +76,23 @@ var fs = {
     * Output: the char at filepointer.index within the data of the file named filepointer.filename
     */
     getFileData: function(filepointer) {
-        if(filepointer.filename.substr(0,4) === "dev/") {
-            var char = fs.data[filepointer.filename][filepointer.index];
-            fs.data[filepointer.filename] =
-                    fs.data[filepointer.filename].substr(1);
+        if(filepointer.fileObject.meta[0] === "c") {
+            var char = filepointer.fileObject.data[filepointer.index];
+            filepointer.fileObject.data =
+                    filepointer.fileObject.data.substr(1);
             filepointer.index = -1;
             return char;
         }
-        if (filepointer.index >=  fs.data[filepointer.filename].length) {
+        if (filepointer.index >=  filepointer.fileObject.data.length) {
             filepointer.eof = -1;
         }
-        return fs.data[filepointer.filename][filepointer.index];
+        return filepointer.fileObject.data[filepointer.index];
     },
     setFileData: function(filepointer, char) {
-        fs.data[filepointer.filename] =
-                fs.data[filepointer.filename].substr(0,filepointer.index) +
+        filepointer.fileObject.data =
+                filepointer.fileObject.data.substr(0,filepointer.index) +
                 char +
-                fs.data[filepointer.filename].substr(filepointer.index+1);
+                filepointer.fileObject.data.substr(filepointer.index+1);
     },
 
 };
