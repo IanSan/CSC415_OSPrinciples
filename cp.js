4ef82499b941285f1930cbe702fd3bf4dfbd627a
Mlienk <greenmonstersheep@gmail.com>
2016-04-14 21:08:59 -0700

    working?

--- cp.js
+++ cp.js
@@ -2,21 +2,15 @@ fs.put("/cp", [
     [open, ["stdout", "w", "fp"]],
     [set, ["buffer", ""]],
     [function(pcb, argv) {
-        var args = pcb.get("argv");
-        args.shift();
-        args = args.join(" ");
-        args = args + "\n";
-        if (args.length > 0) {
-            pcb.set("buffer", args);
-        }
+
         pcb.set("file1", pcb.get("argv")[1])
         pcb.set("file2", pcb.get("argv")[2])
 
     }, []],
-    [open, ["fp", "file1"]],
-    [open, ["fp", "file2"]],
-        [read, ["fp", "file1"]],
-    [write, ["fp", "file2"]],
-    [close, ["file1"]],
-    [close, ["file2"]]
+    [open, ["file1", "r", "fp"]],
+    [open, ["file2", "w", "fp1"]],
+    [read, ["fp", "buffer", 100]],
+    [write, ["fp1", "buffer", 100]],
+    [close, ["fp"]],
+    [close, ["fp1"]]
 ]);
