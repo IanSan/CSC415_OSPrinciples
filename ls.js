33f3c4a596c859c7c025381c0099275f77d4be21
Kevin Valenzuela <kev.jval@gmail.com>
2016-04-19 20:47:43 -0700

    Add ls process

--- ls.js
+++ ls.js
@@ -1,11 +1,28 @@
-ls.js
-
-//list the processes running
-var ls = function (pcb, argv){
-	var ioreq = new IORequest("fileList", pcb);
- 	ioreq.fp = pcb.get(argv[0]);            //_var filepointer
- 	ioreq.data = pcb.get(argv[1]);          //_var stringBuffer
-    ioreq.size = pcb.get(argv[1]).length;   //int size
-    fq.push_back(ioreq);
-	pcb.state = "waiting"; 
-}
+fs.put("/ls", [
+    [open, ["stdout", "w", "fp"]],
+    [function(pcb, argv) {
+            var args = pcb.get("argv");
+            args.shift();
+            if (args.length > 1) {
+                pcb.set("dir", args[0]);
+            } else {
+                pcb.set("dir", pcb.workingdir);
+            }
+    }, []],
+    [readdir, ["dir", "list"]],
+    [function(pcb, argv) {
+            var list = pcb.get("list");
+            var str = "";
+            for (var i = 0; i < list.length; i++) {
+                if (str.length + list[i].length > 80) {
+                    str = str + "\n" + list[i] + " ";
+                } else {
+                    str = str + list[i] + " ";
+                }
+            }
+                    str = str + "\n";
+            pcb.set("buffer", str);
+    }, []],
+    [write, ["fp", "buffer"]],
+    [close, ["fp"]]
+]);
