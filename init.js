2c30070ae4b1d3e56687663d197700c14546032b
Kevin Valenzuela <kev.jval@gmail.com>
2016-04-21 18:08:39 -0700

    Change init process
    Start kernel with init already loaded. Init will
    start device drivers and shell.

--- init.js
+++ init.js
@@ -1,10 +1,21 @@
+var initprocess = [
+    [set, ["wd", "/"]],
+    [chdir, ["wd"]],
+    [set, ["argv", ["/bin/keyboard"]]],
+    [createChildProcess, ["argv"]],
+    [set, ["argv", ["/bin/display"]]],
+    [createChildProcess, ["argv"]],
+    [set, ["argv", ["/bin/shell"]]],
+    [createChildProcess, ["argv"]]
+];
+
+fs.put("/bin/init", initprocess);
+
 function init() {
     //load all processes
-    load(display, "display");
-    load(keyboard, "keyboard");
-    load(shell, "shell");
+    load(initprocess, "init");
 
     //start kernel
     console.log("kernel starting");
     setTimeout(function(){kernel();}, 0);
-}
+};
